e13be3dd46c304a9aa267c07c68951ef1a436956
amahersarosh <mxa69410@ucmo.edu>
2023-01-22 16:53:07 -0600

    Create Q5.py

---
+++ assignment1/Q5.py
@@ -0,0 +1,8 @@
+rad = 30
+_area_of_circle = 3.14*(rad**2)
+print("area of circle :", _area_of_circle)
+_circum_of_circle = 2*3.17*rad
+print("circumference of circle :", _circum_of_circle)
+x = int(input())
+area = 3.14*(x**2)
+print("Area of circle with user input :", area)
