a8bbcdea7073727f13ec1b01e3171da246421da1
amahersarosh <mxa69410@ucmo.edu>
2023-01-22 16:50:29 -0600

    Create Q3.py

---
+++ assignment1/Q3.py
@@ -0,0 +1,13 @@
+sisters = ("Anvi", "Pavi")
+brothers = ("raju", "Shyam", "Ram")
+print("brothers are : ", brothers)
+print("sisters are : ", sisters)
+
+# joining brothers and sisters tuples to siblings
+siblings = brothers+sisters
+print("siblings are : ", siblings)
+print("number of siblings are : ", len(siblings))
+
+# modifying siblings tuple by adding father name and mother name
+family_members = ("father", "mother")+siblings
+print("family members are : ", family_members)
