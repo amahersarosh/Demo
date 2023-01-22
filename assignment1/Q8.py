df060031039445f940ca467bc94960ec292e58a6
amahersarosh <mxa69410@ucmo.edu>
2023-01-22 16:49:06 -0600

    Create Q8.py

---
+++ assignment1/Q8.py
@@ -0,0 +1,6 @@
+#assigned variable radius
+rad=10
+#assigned area of circle formula
+area=3.14 * rad ** 2
+#print the output using string format
+print("The area of Circle with Radius {} is {} meters square".format(rad,area))
