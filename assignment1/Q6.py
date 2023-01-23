33024f725e9f9e8eff0f17d39889d2df0be79ce0
amahersarosh <mxa69410@ucmo.edu>
2023-01-22 18:34:08 -0600

    Create Q6.py

---
+++ assignment1/Q6.py
@@ -0,0 +1,10 @@
+# given string to the variable a
+x="I am a teacher and I love to inspire and teach people"
+# split the given string
+y=x.split()
+# using set to remove duplicates
+seta=set(y)
+# using len to count number of words
+count=len(seta)
+# printing the value
+print(count)
